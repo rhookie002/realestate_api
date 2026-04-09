358b068de88b808d93d043a11d298858840668dc
rhookie002 <59253391+rhookie002@users.noreply.github.com>
2026-04-09 18:01:54 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import path from 'path';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const REALESTATE_API_KEY = process.env.REALESTATE_API_KEY;
+const REALESTATE_API_KEY = process.env.REALESTATE_API_KEY as string,;
 const REALESTATE_BASE_URL = 'https://api.realestateapi.com/v2/PropertySearch';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
