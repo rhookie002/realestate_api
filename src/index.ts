932a1138de9f879fd3c0ad5040b16e12af6fb1a7
rhookie002 <59253391+rhookie002@users.noreply.github.com>
2026-05-05 12:30:21 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ const PORT = process.env.PORT || 3000;
 
 const REALESTATE_API_KEY = process.env.REALESTATE_API_KEY as string;
 const REALESTATE_BASE_URL = 'https://api.realestateapi.com/v2/PropertySearch';
+const apiKey = process.env.GOOGLE_MAPS_API_KEY as string;
 
 // Max records the API will return per single request
 const API_PAGE_SIZE = 250;
